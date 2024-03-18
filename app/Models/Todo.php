af26052375b583ca58c8458006eb824b0ebcf612
Beatrix Devanti Agsi <beatrixdevanti21@gmail.com>
2024-03-18 14:20:14 +0700

    coba

---
+++ app/Models/Todo.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Queue\Failed\PrunableFailedJobProvider;
+
+class Todo extends Model
+{
+    use HasFactory;
+    
+
+    protected $filliable = [
+        'title', 'user_id', 'is_complete'
+    ];
+
+    public function user()
+    {
+        return $this -> belongsTo(User::class);
+    }
+}
